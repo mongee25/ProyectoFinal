00edae3f3e118d17b70a3a9a62811b3b8bf67184
Emiliano Monge <mongee587@gmail.com>
2024-12-02 21:23:08 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/Data/Models/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+
+namespace ProyectoFinal.Data.Models
+{
+    public class Usuarios
+    {
+        public int UsuarioID { get; set; }
+        public string Nombre { get; set; }
+        public string AMaterno { get; set; }
+        public string APaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public string Celular { get; set; }
+        public decimal Altura { get; set; }
+    }
+}
